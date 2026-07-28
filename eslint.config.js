db4d507e30f25312aac9fcbd18f6b2022a2fd09a
Sagor Majomder <sagormajomder@gmail.com>
2026-07-28 14:42:24 +0600

    Implement structural updates and optimizations across multiple modules

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,34 @@
 // eslint.config.js
 import js from '@eslint/js';
 import boundaries from 'eslint-plugin-boundaries';
+import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
-const el = type => ({ element: { type } });
+const dependencyMap = {
+  auth: ['user'],
+  user: [],
+};
 
-const elements = [
-  { type: 'auth', pattern: 'src/modules/auth/**' },
-  { type: 'user', pattern: 'src/modules/user/**' },
-];
+const elements = Object.keys(dependencyMap).map(name => ({
+  type: name,
+  pattern: `src/modules/${name}/**`,
+}));
 
-const dependencyPolicies = [
-  { from: el('auth'), allow: [el('user')] },
-  { from: el('user'), allow: [] },
-  {
-    to: [el('auth'), el('user')],
-    allow: [{ to: { element: { fileInternalPath: '*index.ts' } } }],
-  },
-];
+const policies = Object.entries(dependencyMap).flatMap(([from, deps]) =>
+  deps.map(dep => ({
+    from: { element: { type: from } },
+    allow: {
+      to: { element: { type: dep, fileInternalPath: '*index*' } },
+    },
+  })),
+);
+
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     files: ['src/**/*.ts'],
-    plugins: { boundaries },
+    plugins: { boundaries, import: importPlugin },
     settings: {
       'import/resolver': {
         typescript: { project: './tsconfig.json' },
@@ -31,10 +36,8 @@ export default tseslint.config(
       'boundaries/elements': elements,
     },
     rules: {
-      'boundaries/dependencies': [
-        'error',
-        { default: 'disallow', policies: dependencyPolicies },
-      ],
+      'boundaries/dependencies': ['error', { default: 'disallow', policies }],
+      'import/no-cycle': ['error', { maxDepth: 10, ignoreExternal: true }],
     },
   },
   {
