9811fcf24f19eadf8aaa47bdf1eb27eff42b65c2
Sagor Majomder <sagormajomder@gmail.com>
2026-07-28 16:24:42 +0600

    feat: add asyncCatch utility function and asyncCatchFn type for error handling

---
+++ src/shared/utils/asyncCatch.ts
@@ -0,0 +1,20 @@
+import { logger } from '@src/shared/utils/logger.js';
+import type { asyncCatchFn } from '@src/shared/utils/types.js';
+import type { NextFunction, Request, Response } from 'express';
+
+function asyncCatch(fn: asyncCatchFn): asyncCatchFn {
+  return async function (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      logger.error({ error });
+      next(error);
+    }
+  };
+}
+
+export default asyncCatch;
